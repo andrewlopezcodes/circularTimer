d6285725936a4cd74e494802a280f982ba3a17e8
andrewlopezcodes <andrewlopezcodes@gmail.com>
2020-05-01 19:05:46 -0400

    circularTimer.js - installed Timer class, orgaized countdown timer and attached start and pause button. index.html - created text input, start button, pause button.

--- circularTimer.js
+++ circularTimer.js
@@ -1,1 +1,31 @@
-console.log('hello')
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  }
+  start = () => {
+    this.tick();
+    this.currentTime = setInterval(this.tick, 1000);
+
+  };
+  pause = () => {
+    clearInterval(this.currentTime);
+  };
+  tick = () => {
+    console.log('tick')
+  }
+  durationInput = () => {}
+
+
+}
+
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
