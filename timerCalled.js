50635e00c668fa409a4191b02a4b0aec93d1b02b
andrewlopezcodes <andrewlopezcodes@gmail.com>
2020-05-01 22:44:00 -0400

    index.html - removed stroke-dasharray and strok-dashoffset from circle tag - timerCalled.js - added circle, circlePerimeter, currentDashOffset variables and added functionality to onTick method to create animation of passing time for stroke.

--- timerCalled.js
+++ timerCalled.js
@@ -1,15 +1,25 @@
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
+
+const circlePerimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', circlePerimeter);
+let currentDashOffset = 0;
+
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart() {
     console.log('Timer has started')
   },
   onTick() {
-    console.log('tick')
+    circle.setAttribute('stroke-dashoffset', currentDashOffset);
+    currentDashOffset = currentDashOffset - 50;
   },
   onTimerAtZero() {
     console.log('timer is at 0')
   }
 });
+
+// stroke-dasharray="560"
+//       stroke-dashoffset="-100" 
